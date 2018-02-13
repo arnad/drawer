c7d670bb699130c0b39dee5c0787eae80cf66758
Jason Lantz <jason.lantz@pearson.com>
2018-02-13 13:28:32 -0700

    chore: bob's ∆ for more flexible content section rendering

--- src/js/components/ContentSection.js
+++ src/js/components/ContentSection.js
@@ -1,7 +1,7 @@
 export const ContentSection = ({ contentSectionHandler, children, back, display }) => {
 
   const sectionAnimation = back ? "contentSection slideOutRightContent" : "contentSection slideInRightContent";
-  const stepKids         = children.props.children;
+  const stepKids         = React.Children.map(children.props.children, (child) => child);
   const findBasicViews   = stepKids.filter(c => c.props.myKind === "BasicView");
   const findDetailViews  = stepKids.filter(c => c.props.myKind === "DetailView");
   const findDetailView   = findDetailViews.filter(c => c.props.id === display);
